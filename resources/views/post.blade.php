222c7e7cf1d02459495f099fa661c18fc03e8a8e
Pete Croke <pete@petecroke.com>
2021-03-23 23:45:23 -0400

    initial progress

---
+++ resources/views/post.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>My Blog</title>
+
+    <link rel="stylesheet" href="/app.css">
+
+
+</head>
+
+<body>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur provident vitae voluptate! Dolores et inventore itaque nostrum totam? Architecto dolor earum eum hic molestias nobis quia quos tempore voluptates voluptatum.</p>
+    </article>
+
+    <a href="/">Go Back</a>
+
+</body>
+
+</html>
