2759e73eb5ef69ef022fc4333a8364778c66a444
Pete Croke <pete@petecroke.com>
2021-03-24 00:22:01 -0400

    lesson 8

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,22 +4,17 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>My Blog</title>
+    <title>My Blog Entry</title>
 
     <link rel="stylesheet" href="/app.css">
 
 
 </head>
-
 <body>
     <article>
-        <h1><a href="/post">My First Post</a></h1>
-
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur provident vitae voluptate! Dolores et inventore itaque nostrum totam? Architecto dolor earum eum hic molestias nobis quia quos tempore voluptates voluptatum.</p>
+        <?= $post ?>
     </article>
 
-    <a href="/">Go Back</a>
-
+    <a href="/">Back Home</a>
 </body>
-
 </html>
