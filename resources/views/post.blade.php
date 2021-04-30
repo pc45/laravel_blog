f79cb23dc002e61a74539f61697a12fbce261b57
Pete Croke <pete@petecroke.com>
2021-04-30 13:38:58 -0400

    adding authors to posts and post

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,9 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>My Blog home</title>
+
+    <link rel="stylesheet" href="/app.css">
+
+
+</head>
+
+<body>
 
     <article>
         <h1><?=$post->title;?></h1>
 
         <p>
-            <a href="#">{{$post->user->name}}</a> in <a href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a>
+            <a href="/authors/{{$post->author->username}}">{{$post->author->name}}</a> in <a href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a>
         </p>
 
         <div>
@@ -14,3 +28,5 @@
 
     <a href="/">Back Home</a>
 
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Models\Category;
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
@@ -23,7 +24,7 @@ Route::get('/', function () {
     });
 */
     return view('posts',[
-        'posts' => Post::with('category')->get()
+        'posts' => Post::latest()->get()
     ]);
 });
 
@@ -36,5 +37,11 @@ Route::get('posts/{post:slug}', function (Post $post) {
 Route::get('categories/{category:slug}', function (Category $category) {
     return view('posts',[
         'posts' => $category->posts
+    ]);
+});
+
+Route::get('authors/{author:username}', function (User $author) {
+    return view('posts',[
+        'posts' => $author->posts
     ]);
 });
