07493767276931ea9d777eba3a9eb5a850dc9294
Pete Croke <pete@petecroke.com>
2021-05-14 16:24:39 -0400

    create category functionality

--- routes/web.php
+++ routes/web.php
@@ -24,24 +24,29 @@ Route::get('/', function () {
     });
 */
     return view('posts',[
-        'posts' => Post::latest()->get()
+        'posts' => Post::latest()->get(),
+        'categories' => Category::all(),
     ]);
 });
 
 Route::get('posts/{post:slug}', function (Post $post) {
     return view('post', [
         'post' => $post,
+        'categories' => Category::all(),
     ]);
 });
 
 Route::get('categories/{category:slug}', function (Category $category) {
     return view('posts',[
-        'posts' => $category->posts
+        'posts' => $category->posts,
+        'currentCategory' => $category,
+        'categories' => Category::all(),
     ]);
 });
 
 Route::get('authors/{author:username}', function (User $author) {
     return view('posts',[
-        'posts' => $author->posts
+        'posts' => $author->posts,
+        'categories' => Category::all(),
     ]);
 });
