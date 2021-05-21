6d98d7ccc43e28fb6103f69a286252aa2cf1da15
Pete Croke <pete@petecroke.com>
2021-05-21 16:09:25 -0400

    start search functionality

--- routes/web.php
+++ routes/web.php
@@ -18,13 +18,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-/*
-    DB::listen(function ($query){
-       logger($query->sql, $query->bindings);
-    });
-*/
+
+    $posts = Post::latest();
+
+    if(request('search')) {
+        $posts
+            ->where('title', 'like', '%'.request('search').'%')
+            ->orWhere('body', 'like', '%'.request('search').'%');
+    }
+
     return view('posts',[
-        'posts' => Post::latest()->get(),
+        'posts' => $posts->get(),
         'categories' => Category::all(),
     ]);
 })->name('home');
